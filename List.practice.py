e22c0ebf2fa0475890b925edc962180407e16b7b
mimi73727170-coder <mimi73727170@gmail.com>
2026-05-18 15:28:58 +0530

    Add files via upload

---
+++ List.practice.py
@@ -0,0 +1,11 @@
+names = []
+n1 = input("Enter name 1:")
+n2 = input("Enter name 2:")
+n3 = input("Enter name 3:")
+names.append(n1)
+names.append(n2)
+names.append(n3)
+print("Names in list :")
+for name in names :
+    print(name)
+
