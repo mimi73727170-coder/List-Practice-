fffb95dd55463ddd9bd1d8f352d585e0b54fb52d
mimi73727170-coder <mimi73727170@gmail.com>
2026-05-18 17:19:09 +0530

    Add files via upload

---
+++ List(practice).py
@@ -0,0 +1,19 @@
+student = {
+    "name" : "Soniya",
+    "age" : 16,
+    "city" : "Delhi"
+}
+print(student)
+print(student["name"])
+print(student["age"])
+student[("age")] = 17
+print(student)
+student["school"] = "ABC school"
+print(student)
+book = {
+    "title" :"python",
+    "price" : 500 ,
+    "author" : "john"
+}
+print (book)
+book["pages"] = 200
